2a76f0e4d7d5803491c3148a73580cdcdb426179
darlenewagner <ydn3@cdc.gov>
2024-09-10 15:46:51 -0400

    Prepare hash of arrays for parsing rotavirus segments.

--- Rotavirus_BlastN_Contig_Match.pl
+++ Rotavirus_BlastN_Contig_Match.pl
@@ -1,7 +1,8 @@
 #!/usr/bin/perl
 use strict;
 use Getopt::Long;
-## Adapted to curate BlastN matches to rotavirus or influenzavirus segments.
+use warnings;
+use Data::Dumper;
 ## Input BlastN file is tab-delimited with 'virus' or '10239' in 17th column
 ## Output format option from original BlastN command had 18 columns:
 ## -outfmt '6 qseqid pident length qlen slen mismatch gapopen qstart qend sstart send evalue bitscore sgi sacc staxids sblastnames stitle'
@@ -14,23 +15,38 @@ my $accession = '';
 my $virusName = '';
 
 ## For searching when $accession or $virusName are not supplied
-my $highConsequenceStr = qr/HIV\-(1|2)\sisolate\s.+,\scomplete\sgenome/;
+my $highConsequenceStr = qr/(HIV\-(1|2)\sisolate\s.+,\scomplete\sgenome|Human\simmunodeficiency\svirus\s(1|2)\sproviral)/;
 my @HighConsequenceArray = ();
-my @FindRotaSegments = ();
+my %Sortable;
+
+## A Hash of arrays for organizing rotavirus segment matches
+my %RotavirusSegments = { 'segment_1' => [],
+			  'segment_2' => [],
+			  'segment_3' => [],
+			  'segment_4' => [],
+			  'segment_5' => [],
+			  'segment_6' => [],
+			  'segment_7' => [],
+			  'segment_8' => [],
+			  'segment_9' => [],
+			  'segment_10' => [],
+			  'segment_11' => []
+                      };
 
 $accession = $ARGV[0];
 
+
+## Help section and usage
 my $help = '';
 my $verbose = '';
-my $advanced = '';
-my $segments = '';
-my $isRotavirus = 0;
+my $sortIt = '';
+my $rotavirus = '';
 
 GetOptions(
     'help|h|'  => \$help,
     'verbose'  => \$verbose,
-    'segment'  => \$segments,
-    'highConsequence' => \$advanced,
+    'sort'     => \$sortIt,
+    'rotavirus|r|' => \$rotavirus,
     ) or die "Error in command line option arguments\n";
 
 if($help)
@@ -39,24 +55,32 @@ if($help)
        print "\n<------------------------------------------------- Options -------------------------------------------------->\n";
        print "ACCESSION or VIRUS NAME STR#, Search key (required)\n";
        print "--verbose,                    Output column headers (optional)\n";
-       print "--segment,                    Cluster output by rotavirus segement (input string must contain 'Rotavirus')";
-       print "--highConsequence,            Search for HIV-1 or other high-consequence pathogens (optional)\n";
+       print "--sort,                       Sort output by MatchID (NCBI Accession) and AlnLength\n";
+       print "--rotavirus,                  Search for HIV-1 or other high-consequence pathogens (no search key needed)\n";
        print "--help,                       Display this help message\n\n";
        exit;
    } 
 
+
+## Determine input string '$accession' type
+
 if($accession =~ /^[A-Z][a-z]+\s+([A-Za-z][A-Za-z0-9]?[0-9]?\s)?([A-Za-z0-9]+(-|_)?[A-Za-z0-9]+|sp|str|strain)\.?\s+([A-Z]\s)?[A-Za-z0-9]+(-|_)?[A-Za-z0-9]+/)
   {
-      if($accession =~ /Rotavirus/i)
-        {
-	    $isRotavirus = 1;
-	}
     $virusName = $ARGV[0];
      if($verbose)
         {
 	    print "Search by species/strain name:\n";
 	    print "QueryID\t%Ident.\tAlnLength\tMatchID\tMatchName\n";
 	}
+  }
+elsif($accession =~ /^Human\srotavirus\s(A|B|C|D|E|F|G)/)
+  {
+      $virusName = $ARGV[0];
+      if($verbose)
+      {
+	   print "Search Human rotavirus without categorization of segments:\n";
+	   print "QueryID\t%Ident.\tAlnLength\tMatchID\tMatchName\n";
+      }
   }
 elsif($accession =~ /^[A-Z][A-Z][A-Z0-9]+\.?[0-9]$/)
   {
@@ -74,15 +98,17 @@ elsif($accession =~ /^N(C|T|X|Z)_[0-9]+\.?[0-9]$/)
 	  print "QueryID\t%Ident.\tAlnLength\tMatchID\tMatchName\n";
        }
   }
-elsif(!$advanced)
+elsif(!$rotavirus)
   {
-      die "Search string too short and/or lacks sufficient information. $!";
+      die "Search string too short and/or lacks sufficient information --$!";
   }
 
 
+## Iterate through blastN output
+## Must be tabular format with:
+## -outfmt '6 qseqid pident length qlen slen mismatch gapopen qstart qend sstart send evalue bitscore sgi sacc staxids sblastnames stitle'
 
 
-my @multi_segment = ();
 my $i = 0;
 my @line = ();
 
@@ -94,50 +120,93 @@ while(<STDIN>)  ## Read BlastN file, use mode according to which string is nonem
 	    if($line[16] =~ /virus|10239/)
 	    {
 		chomp $line[17];
-		 my $newLine = $line[0]."\t".$line[1]."\t".$line[2]."\t".$line[14]."\t".$line[17]."\n";
-		 if($newLine =~ m/$accession/)
+		my $newLine = '';
+		if($verbose)
 		  {
-		     print $newLine;
+		    $newLine = $line[0]."\t".$line[1]."\t".$line[2]."\t".$line[14]."\t".$line[17]."\n";
 		  }
-		 elsif(($newLine =~ m/$highConsequenceStr/) && ($advanced))
+		else
+		 {
+                     $newLine = $line[0]."\t\t".$line[1]."\t\t".$line[2]."\t\t".$line[14]."\t\t".$line[17]."\n";
+		 }
+		if($newLine =~ m/$accession/)
+		  {
+               	    if($sortIt)
+		      {
+			  $Sortable{$line[14]}{$line[2]} = $newLine;
+		      }
+		    else
+		      {
+			  print $newLine;
+		      }
+		  }
+		 elsif(($newLine =~ m/$highConsequenceStr/) && ($rotavirus))
 	          {
 		    push @HighConsequenceArray, $newLine;
 		 }
 	     }
 	}   
-      elsif($virusName =~ /^[A-Z][a-z]+\s+([A-Za-z][A-Za-z0-9]?[0-9]?\s)?([A-Za-z0-9]+(-|_)?[A-Za-z0-9]+|sp|str|strain)\.?\s+([A-Z]\s)?[A-Za-z0-9]+(-|_)?[A-Za-z0-9]+/)
+      elsif($virusName =~ /^[A-Z][a-z]+\s+([A-Za-z][A-Za-z0-9]?[0-9]?\s)?([A-Za-z0-9]+(-|_)?[A-Za-z0-9]+|sp|str|strain)\.?\s+([A-Z]\s)?[A-Za-z0-9]+(-|_)?[A-Za-z0-9]+/ || $virusName =~ /Human\srotavirus\s(A|B|C|D|E|F|G)/)
         {
 	    @line = split(/\t/, $_);
 	    if($line[16] =~ /virus|10239/)
 	    {
 		chomp $line[17];
-		my $newLine = $line[0]."\t".$line[1]."\t".$line[2]."\t".$line[14]."\t".$line[17]."\n";
-		if(($newLine =~ m/$virusName/) && !$segments)
+		my $newLine = '';
+		if($verbose)
+		 {
+		    $newLine = $line[0]."\t".$line[1]."\t".$line[2]."\t".$line[14]."\t".$line[17]."\n";
+		 }
+		else
+		 {
+                     $newLine = "'".$line[0]."'\t'".$line[1]."'\t'".$line[2]."'\t'".$line[14]."'\t'".$line[17]."'\n";
+		 }
+		if($newLine =~ m/$virusName/)
 		{
-		    print $newLine;
-		
+		    #print $newLine;
+		   if($sortIt)
+		      {
+			  $Sortable{$line[14]}{$line[2]} = $newLine;
+		      }
+		    else
+		      {
+                         print $newLine
+		      }
+
 		}
-		elsif(($isRotavirus == 1) && $segments)
-		{
-		    push @FindRotaSegments, $newLine;
-		}
-		elsif(($newLine =~ m/$highConsequenceStr/) && ($advanced))
+		elsif(($newLine =~ m/$highConsequenceStr/) && ($rotavirus))
 	        {
                    push @HighConsequenceArray, $newLine;
 		}
 	    }
      	}
-      elsif(($advanced) && ($virusName !~ /^[A-Z][a-z]+\s+[A-Za-z0-9]+(-|_)?[A-Za-z0-9]+\.?\s+[A-Za-z0-9]+(-|_)?[A-Za-z0-9]+/) && ($accession !~ /^[A-Z][A-Z][A-Z0-9]+\.?[0-9]$/) && ($accession !~ /^N(C|T|X|Z)_[0-9]+\.?[0-9]$/))
+      elsif(($rotavirus) && ($virusName !~ /^[A-Z][a-z]+\s+[A-Za-z0-9]+(-|_)?[A-Za-z0-9]+\.?\s+[A-Za-z0-9]+(-|_)?[A-Za-z0-9]+/) && ($accession !~ /^[A-Z][A-Z][A-Z0-9]+\.?[0-9]$/) && ($accession !~ /^N(C|T|X|Z)_[0-9]+\.?[0-9]$/))
         {
 	    @line = split(/\t/, $_);
       	    if($line[16] =~ /virus|10239/)
 	    {
 		chomp $line[17];
-        	my $newLine = $line[0]."\t".$line[1]."\t".$line[2]."\t".$line[14]."\t".$line[17]."\n";
+		my $newLine = '';
+
+		if($verbose)
+		 {
+                    $newLine = $line[0]."\t".$line[1]."\t".$line[2]."\t".$line[14]."\t".$line[17]."\n";
+		 }
+		else
+		 {
+ 		    $newLine = "'".$line[0]."'\t'".$line[1]."'\t'".$line[2]."'\t'".$line[14]."'\t'".$line[17]."'\n";
+                 }
+		
       		if($newLine =~ m/$highConsequenceStr/)
       		{
                     push @HighConsequenceArray, $newLine;
+                    
+		    if($sortIt)
+		      {
+			  $Sortable{$line[14]}{$line[2]} = $newLine;
+		      }
       		}
+
       	    }
 	}
       #else
@@ -146,7 +215,24 @@ while(<STDIN>)  ## Read BlastN file, use mode according to which string is nonem
       #}
   }
 
-if($advanced)
+## Option '--sort' with search string included
+
+if($sortIt && !$rotavirus)
+{
+    foreach my $acc (sort keys %Sortable)
+    {
+	foreach my $aln (sort { $b <=> $a } keys %{ $Sortable{$acc}} )
+	{
+	    print $Sortable{$acc}{$aln};
+	}
+    }
+    
+}
+
+
+## When option '--highConsequence' is used, no search string is needed
+
+if($rotavirus)
   {
       if($verbose)
         {
@@ -154,9 +240,19 @@ if($advanced)
            print "QueryID\t%Ident.\tAlnLength\tMatchID\tMatchName\n";
 	}
       my $i = 0;
-      foreach(@HighConsequenceArray)
+      if($sortIt)
+        {
+	    foreach my $acc (sort keys %Sortable)
+	    {
+		foreach my $aln (sort { $b <=> $a } keys %{ $Sortable{$acc}} )
+		{
+		    print $Sortable{$acc}{$aln};
+		}
+	    }
+        }
+      else
       {
-	  print $HighConsequenceArray[$i];
-	  $i++;
+         die "Sort option required for rotavirus search --\n";
       }
   }
+
