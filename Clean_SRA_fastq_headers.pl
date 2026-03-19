99ea2e6e0c25693b83c01dd5b3a6b710cfb6582c
darlenewagner <ydn3@cdc.gov>
2026-03-19 15:06:40 -0400

    SRA cleaing script with GetOptions for strand label

--- Clean_SRA_fastq_headers.pl
+++ Clean_SRA_fastq_headers.pl
@@ -3,5 +3,33 @@ use strict;
 use Getopt::Long;
 use warnings;
 
+my $strand = '1:N:0:1';
+
+GetOptions(
+    'strand|s|=s' => \$strand,
+    ) or die "Error in command line option argument\n";
+
+if($strand !~ /^(1|2)\:/)
+{
+    die "Error: The first integer in '--strand' argument must be 1 or 2\n";
+}
 
 
+while(<STDIN>)
+{
+    if(($. % 4) == 1)
+     {
+	my $ws = index($_, ' '); 
+	my $id = substr($_, 0, $ws);
+	print $id, " ", $strand, "\n";
+     }
+    elsif(($. % 4) == 3)
+     {
+	 my $first = substr($_, 0, 1);
+	 print $first, "\n";
+     }
+    else
+     {
+	print;
+     }
+}
