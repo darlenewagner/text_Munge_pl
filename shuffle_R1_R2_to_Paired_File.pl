86421f5f5ebb704872b27dbf354b3acc79c6a9d3
darlenewagner <ydn3@cdc.gov>
2026-03-23 10:46:10 -0400

    Replace ROSS

--- shuffle_R1_R2_to_Paired_File.pl
+++ shuffle_R1_R2_to_Paired_File.pl
@@ -2,19 +2,36 @@
 
 use Bio::SeqIO;
 use strict;
-my $R1_File_Name = @ARGV[1];
-my $R2_File_Name = @ARGV[2];
+use warnings;
 
-my @baseFileName = split(/(_R1|_1\.)/, $R1_File_Name);
+# Shuffle reads from forward and reverse reads in .fastq
+# Validate that there are 2 input files and 1 output file
+if(@ARGV != 3) {
+    die "Usage: $0 Reads_R1_001.fastq Reads_R2_001.fastq Reads_Pairs.fastq\n";
+}
+
+my ( $R1_File_Name, $R2_File_Name, $Paired_File_Name ) = @ARGV;
+
+
 
 
 my $R1 = Bio::SeqIO->new(
-     -format  => 'fastq-illumina',
+     -format  => 'fastq',
+     -variant => 'illumina',
      -file    => $R1_File_Name);
 
+my $R2 = Bio::SeqIO->new(
+    -format  => 'fastq',
+    -variant => 'illumina',
+    -file    => $R2_File_Name);
+
 my $out = Bio::SeqIO->new(
-    -format  => 'fastq-illumina',
-    -file    => $baseFileName[0]);
+    -format  => 'fastq',
+    -variant => 'illumina',
+    -file    => ">$Paired_File_Name");
 
+while(my $seqR1 = $R1->next_seq){
+    $out->write_seq($seqR1);
+  }
 
-
+exit;
