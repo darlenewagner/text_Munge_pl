178fd1cfc7dfcb119fd338f6af95337aa45cc100
darlenewagner <ydn3@cdc.gov>
2026-05-27 11:05:04 -0400

    For read mapping statistics.

--- calculate_Quartiles_and_Median.pl
+++ calculate_Quartiles_and_Median.pl
@@ -3,19 +3,6 @@ use Statistics::Lite;
 
 ## split fasta header at input string, $trimmer, to shorten header in fasta input file, $fasta
 
-my $trimmer = $ARGV[0];
-
-chomp $trimmer;
-
-if($trimmer eq '-s')
-  {
-      $trimmer = '\s';
-  }
-
-my $fasta = $ARGV[1];
-
-open(FASTA, $fasta) || die "Can't find fasta-formatted input file, $fasta $!";
-
 
 while(<FASTA>)
 {
