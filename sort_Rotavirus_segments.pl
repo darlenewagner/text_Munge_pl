cfcf5d98ad95d0da9690edf562da0d2fca3e3d62
darlenewagner <ydn3@cdc.gov>
2026-06-15 14:57:36 -0400

    Sort Rotavirus segments and/or splice with Ns

---
+++ sort_Rotavirus_segments.pl
@@ -0,0 +1,61 @@
+use strict;
+use Getopt::Long;
+use warnings;
+
+## Expects a multifasta of Rotavirus (or Influenza) segments
+## chomp nucleotide sequence lines and sort fasta records by links
+
+my $reference = 'N';
+
+## Say '--reference Y' to create a spliced Rotavirus genome reference
+my $spliceReference = GetOptions(
+    'reference' => \$reference,
+    ) or die "Error in command line option argument\n";
+
+my $fasta = $ARGV[0];
+
+open(FASTA, $fasta) || die "Can't find fasta-formatted input file, $fasta $!";
+
+my @GENOME = <FASTA>;
+
+my %Segments = {};
+
+#my %sortedSegments {};
+
+my @Titles = ();
+
+my $i = 0;
+my $prev = 0;
+
+foreach(@GENOME)
+{
+     if($GENOME[$i] =~ /^>/)
+     {
+	$prev = $i;
+     }
+     elsif($GENOME[$i] =~ /^[ACGNT]+/)
+     {
+	 chomp $GENOME[$i];
+	 $Segments{$GENOME[$prev]} = $Segments{$GENOME[$prev]}.$GENOME[$i];
+     }
+
+     $i++;
+ }
+
+my @sortedKeys = sort {length($Segments{$b}) <=> length($Segments{$a})} keys(%Segments);
+
+
+my $k = 0;    
+    
+foreach(@sortedKeys)
+ {
+     print $sortedKeys[$k];
+     for(my $line = 0; $line < length($Segments{$sortedKeys[$k]}); $line = $line + 60)
+      {
+        print substr($Segments{$sortedKeys[$k]}, $line, 60), "\n"; 
+      }
+     #print $Segments{$sortedKeys[$k]}, "\n";
+     $k++;
+ }
+
+print "\n";
