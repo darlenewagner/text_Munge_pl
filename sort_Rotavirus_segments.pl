ea1e0dd866c2bbf039b126f547dd7ba3f653f922
darlenewagner <ydn3@cdc.gov>
2026-06-15 15:48:41 -0400

    Enable spliced Rotavirus genome with spacer.

--- sort_Rotavirus_segments.pl
+++ sort_Rotavirus_segments.pl
@@ -5,11 +5,11 @@ use warnings;
 ## Expects a multifasta of Rotavirus (or Influenza) segments
 ## chomp nucleotide sequence lines and sort fasta records by links
 
-my $reference = 'N';
+my $reference = '';
 
 ## Say '--reference Y' to create a spliced Rotavirus genome reference
-my $spliceReference = GetOptions(
-    'reference' => \$reference,
+GetOptions(
+    'reference|r|' => \$reference,
     ) or die "Error in command line option argument\n";
 
 my $fasta = $ARGV[0];
@@ -31,7 +31,8 @@ foreach(@GENOME)
 {
      if($GENOME[$i] =~ /^>/)
      {
-	$prev = $i;
+	 $prev = $i;
+	 $Segments{$GENOME[$prev]} = '';
      }
      elsif($GENOME[$i] =~ /^[ACGNT]+/)
      {
@@ -45,17 +46,44 @@ foreach(@GENOME)
 my @sortedKeys = sort {length($Segments{$b}) <=> length($Segments{$a})} keys(%Segments);
 
 
-my $k = 0;    
+my $spacer = "NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN";
+
+
+if($reference)
+{
+    my $header = '';
+    my $sequence = '';
     
-foreach(@sortedKeys)
- {
+    for(my $count = 0; $count < scalar @sortedKeys; $count++)
+      {
+	  if($count == 0)
+	    {
+	      $header = $sortedKeys[$count];
+  	      $sequence = $Segments{$sortedKeys[$count]}.$spacer;
+	    }
+	  elsif($count < 10)
+	  {
+	      $sequence = $sequence.$Segments{$sortedKeys[$count]}.$spacer;
+	  }
+	  else
+	  {
+             $sequence = $sequence.$Segments{$sortedKeys[$count]}."\n";
+	  }
+      }
+    print $header, $sequence;
+  }
+else
+  {
+  my $k = 0;    
+  foreach(@sortedKeys)
+   {
      print $sortedKeys[$k];
      for(my $line = 0; $line < length($Segments{$sortedKeys[$k]}); $line = $line + 60)
       {
-        print substr($Segments{$sortedKeys[$k]}, $line, 60), "\n"; 
+        print substr($Segments{$sortedKeys[$k]}, $line, 60), "\n";
       }
      #print $Segments{$sortedKeys[$k]}, "\n";
      $k++;
+    }
+   print "\n";
  }
-
-print "\n";
